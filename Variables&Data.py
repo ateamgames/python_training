7131eb8aa3166b0f067cdf387ae49620e0fc6121
Adrian <adrian@ateam.games>
2019-07-29 16:50:30 +0200

    Updating Variables and data

---
+++ Variables&Data.py
@@ -0,0 +1,19 @@
+#Using variables makes the programs easier to change in time
+#print("There was once a man named Jerry")
+#print("He was 50 years old")
+#print("He really liked his name Jerry")
+#print("He didn't like being 50")
+
+#an example of how the variables are used in a program
+
+character_name = "Jerry"   # this is a variable called character_name, it's value is Jerry (a string)
+character_age = "50"         # this is a variable called character_age, it's value is also a string
+print("There was once a man named " + character_name + ", ")
+print("He was " + character_age + " years old")
+print("He really liked his name " + character_name + ", ")
+print("He didn't like being " + character_age + ", ")
+
+#any time you want to change the name/age, you will only have to change the variable values
+
+
+
