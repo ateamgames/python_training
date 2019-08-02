d5fe67043b0ef6db94879642a340c42dff5c02f7
Adrian <adrian@ateam.games>
2019-08-02 17:22:36 +0200

    strings

---
+++ working_with_strings.py
@@ -0,0 +1,20 @@
+print('testing the string')
+
+print('testing the\n string')  # \n creates a new line
+
+print('testing the\" string') # \" prints the cotation mark
+
+phrase = "testing the string"
+print(phrase + "is cool") # concatenation, it appends 2 strings togheter
+
+print(phrase.lower()) # this is using a function called .lower and it will convert all characters within the string into lower case
+print(phrase.isupper()) # test the string if it's upper or not ( bolean answer, true or false)
+
+print(phrase.upper().isupper()) # multiple functions running on a string
+
+print(len(phrase)) # len = lenght, it tells how many characters are inside the string
+
+print(phrase[0]) # [you can add an index here] and it will spit out the letter/character on that specific position. 0 is the first!!!!
+
+
+
