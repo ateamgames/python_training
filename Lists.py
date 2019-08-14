51a45ac3599bca4c0712f63ab624cea6ee957767
Adrian <adrian@ateam.games>
2019-08-14 17:10:29 +0200

    Working with lists

---
+++ Lists.py
@@ -0,0 +1,8 @@
+friends = ["Kevin", "Karen", "Jim"]
+
+print(friends[2]) # this prints the element with index 2. Index starts from 0
+                  # if you intex -1, it will print out the last item of the list
+                  # you can also use ranges such as [1:3]
+                  # it can also grab everythnig after an index, [1:]
+friends[1] = "Mike" # this will update the value on index 1 with a new value, called Mike
+
