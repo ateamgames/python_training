b689b289793a35a317d4be081b8e5db019c2d97b
Adrian <adrian@ateam.games>
2019-08-23 14:43:11 +0200

    Functions in Python

---
+++ FunctionsInPython.py
@@ -0,0 +1,17 @@
+def sayhi ():
+    print("Hello user")
+# creates a function called sayhi. This will not do anything unless is called
+
+
+print("Top")
+sayhi()
+print("Bottom")
+
+# the flow of the top 3 commands shows that the function is called in between the print statements. This shows, how functions are called
+
+def say_hi(name):
+    print("Hello " + name)
+
+# a slightly more advance function that has a parameter called name. The function will need to input a name in order to work
+
+say_hi("Steve")
