48befbc3e3a3f8fd090d5debc72558809cb4d751
Adrian <adrian@ateam.games>
2019-08-23 16:07:28 +0200

    IfStatements

---
+++ IfStatements.py
@@ -0,0 +1,18 @@
+is_male = True
+is_tall = True
+
+if is_male:
+    print("You are a male")
+else:
+    print("you are not a male")
+
+# Prints out that you are a male if is_male is TRUE, otherwise it will printout the else statement
+
+if is_male or is_tall:
+    print("You are a male or you are tall, or both, cant'decide")
+
+# the OR function there is translated as "either", you can be male, tall, or both and it will printout the result
+# Another useful statement is "and" where all if statements need to be True in order for it to print the result
+# you can also use elif as a statement that combines else if in your code. Help to check specific logic of your program
+
+  
