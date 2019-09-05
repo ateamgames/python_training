244abedfb84288b773eefe96f82eb6dbcd6768fd
Adrian <adrian@ateam.games>
2019-09-05 12:02:35 +0200

    While Loop

---
+++ WhileLoop.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+print("Done with the loop")
+
+# a while loop will always go back to the condition to check if it needs to run again
+# once the condition is not meet anymore, the while loop stops
+# in this case, the condition is that i will <= to 10
