70b2edc3a33b807f3dfb30a3abf740db8473cba4
Adrian <adrian@ateam.games>
2019-09-05 14:56:44 +0200

    For Loop

---
+++ ForLoop.py
@@ -0,0 +1,10 @@
+for letter in "Giraffe Academy":
+    print(letter)
+
+
+friends = ["Juim", "Mark", "Kevin"]
+for index in range(5):
+    if index == 0:
+        print("First")
+    else:
+        print("Not")
